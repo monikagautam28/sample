7cd47eb73428acd6e273e6b5a35d9f6fb670341e
Monika Gautam <79004366+monikagautam28@users.noreply.github.com>
2022-05-27 11:06:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
+const userData = [
+  { name: "monika" },
+  { name: "kirti" },
+  { name: "shalini" },
+  { name: "gitu" },
+];
 
 function App() {
+  const [user, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(userData);
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, checked } = e.target;
+    if (name === "allselect") {
+      let tempUser = user.map((user,i) => {
+        return { ...user, ischecked: checked };
+      });
+
+      setUsers(tempUser);
+    } else {
+      let tempUser = user.map((user,i) =>
+        user.name === name ? { ...user, ischecked: checked } : user
+      );
+      setUsers(tempUser);
+    }
+  };
+
+const deletefunction = (e)=>{
+console.log(e)
+
+
+}
+  const deleteItem =()=>{
+
+let tempUser = user.filter((item)=>{ return item.ischecked !== true})
+console.log(tempUser)
+tempUser.forEach((e)=>{
+  deletefunction(e)
+})
+
+setUsers(tempUser)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-4" style={{ width: 400 }}>
+      <div className="form">
+        <h3>selectusers</h3>
+        <div className="form-check">
+          <input
+            type="checkbox"
+            classname="form-check-input"
+            name="allselect"
+            checked={user.filter((user)=>user?.ischecked !== true).length<1}
+            onChange={handleChange}
+          />
+          <label className="form-check-label ms-2">All select</label>
+        </div>
+        {user.map((user) => (
+          <div className="form-check">
+            <input
+              type="checkbox"
+              classname="form-check-input"
+              onChange={handleChange}
+              checked={user?.ischecked || false}
+              name={user.name}
+            />
+            <label className="form-check-label ms-2"> {user.name}</label>
+          </div>
+        ))}
+      </div>
+      <button onClick={deleteItem}>delete</button>
     </div>
   );
 }
 
 export default App;
+
